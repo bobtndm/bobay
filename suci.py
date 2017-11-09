300645af6e4ba85035581a8a81bbb4fb8099de67
bobtndm <30938318+bobtndm@users.noreply.github.com>
2017-11-09 23:56:38 +0700

    Update suci.py

--- suci.py
+++ suci.py
@@ -104,9 +104,9 @@ helo=""
 KAC=[cl,ki]
 #KAC=[cl]AC=[cl,ki,ki2,ki3,ki4,ki5,ki6,ki7,ki8,ki9,k1,k2,k3,k4,k5,k6,k6,k8,k9,w1,w2,w3,w4,w5,w6,w7,w8,w9,l1,l2,l3,l4,l5]
 mid = cl.getProfile().mid
-Bots = ["uf43eb623c4b246c45690bf19e048af05","u0e495c4debaa244e5f61f4e090950f75"]
-admsa = "u0e495c4debaa244e5f61f4e090950f75"
-admin = "u0e495c4debaa244e5f61f4e090950f75"
+Bots = [mid,"uf43eb623c4b246c45690bf19e048af05"]
+admsa = "uf43eb623c4b246c45690bf19e048af05"
+admin = "uf43eb623c4b246c45690bf19e048af05"
 
 wait = {
     'contact':False,
